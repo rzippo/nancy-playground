a009bc72416447dacee89e9c731e1631bb161aff
Raffaele Zippo <raffaele.zippo@ing.unipi.it>
2025-04-01 16:17:45 +0200

    Draft plot data structures

--- Nancy-Playground/MppgParser/Statements/PlotCommand.cs
+++ Nancy-Playground/MppgParser/Statements/PlotCommand.cs
@@ -1,18 +1,35 @@
-﻿namespace Unipi.MppgParser;
+﻿using Unipi.Nancy.Expressions;
+
+namespace Unipi.MppgParser;
 
 public class PlotCommand : Statement
 {
+    public List<Expression> FunctionsToPlot { get; init; } = [];
+    public PlotSettings Settings { get; init; } = new();
+    
     public override string Execute(State state)
     {
-        return "Not implemented";
+        return "Plotting is not implemented in this context.";
     }
 
     public override StatementOutput ExecuteToFormattable(State state)
     {
-        return new StatementOutput
+        var functions = FunctionsToPlot
+            .Select(ex =>
+            {   
+                ex.ParseTree(state);
+                if(ex.NancyExpression is CurveExpression ce)
+                    return ce.Compute();
+                else
+                    throw new Exception("Cannot plot a number.");
+            })
+            .ToList();
+        return new PlotOutput
         {
+            FunctionsToPlot = functions,
+            Settings = Settings,
             StatementText = Text,
-            OutputText = "Not implemented"
+            OutputText = "If you are reading this, the formatter does not implement plots."
         };
     }
 }
