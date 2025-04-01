90c7973b57d95fee6c26c461f02f53e1ade12601
Raffaele Zippo <raffele.zippo@ing.unipi.it>
2025-04-01 16:17:45 +0200

    Draft plot data structures

---
+++ Nancy-Playground/MppgParser/Statements/PlotOutput.cs
@@ -0,0 +1,9 @@
+﻿using Unipi.Nancy.MinPlusAlgebra;
+
+namespace Unipi.MppgParser;
+
+public class PlotOutput : StatementOutput
+{
+    public List<Curve> FunctionsToPlot { get; init; } = [];
+    public PlotSettings Settings { get; init; } = new();
+}
---
+++ Nancy-Playground/MppgParser/Statements/PlotSettings.cs
@@ -0,0 +1,46 @@
+﻿using Unipi.Nancy.Numerics;
+
+namespace Unipi.MppgParser;
+
+public record PlotSettings
+{
+    /// <summary>
+    /// The graph title.
+    /// </summary>
+    public string Title { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Range for the x-axis.
+    /// </summary>
+    public (Rational?, Rational?) XLimit { get; init; } = (null, null);
+
+    /// <summary>
+    /// Range for the y-axis.
+    /// </summary>
+    public (Rational?, Rational?) YLimit { get; init; } = (null, null);
+
+    /// <summary>
+    /// Label for the x-axis.
+    /// </summary>
+    public string XLabel { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Label for the y-axis.
+    /// </summary>
+    public string YLabel { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Name of the png file to save the plot to.
+    /// </summary>
+    public string OutPath { get; init; } = string.Empty;
+
+    /// <summary>
+    /// If false, removes the grid from the plot.
+    /// </summary>
+    public bool Grid { get; init; } = true;
+
+    /// <summary>
+    /// If false, remove the background from the plot.
+    /// </summary>
+    public bool Background { get; init; } = true;
+}
