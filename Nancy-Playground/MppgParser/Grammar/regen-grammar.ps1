ff9c1b9e9ce7ce43c9375de451d861733e9fe72e
Raffaele Zippo <raffele.zippo@ing.unipi.it>
2025-04-03 12:03:50 +0200

    Update regen script

--- Nancy-Playground/MppgParser/Grammar/regen-grammar.ps1
+++ Nancy-Playground/MppgParser/Grammar/regen-grammar.ps1
@@ -1,9 +1,14 @@
-﻿if(Get-Command antlr4 -ErrorAction SilentlyContinue)
-{
-    antlr4 -Dlanguage=CSharp -o ./ -package Unipi.MppgParser.Grammar -visitor -no-listener -lib ./ ./Mppg.g4
+﻿Push-Location $PSScriptRoot
+try {
+    if(Get-Command antlr4 -ErrorAction SilentlyContinue)
+    {
+        antlr4 -Dlanguage=CSharp -o ./ -package Unipi.MppgParser.Grammar -visitor -no-listener -lib ./ ./Mppg.g4
+    }
+    else
+    {
+        Write-Host "ANTLR is not installed. Follow the instructions at https://github.com/antlr/antlr4/blob/master/doc/getting-started.md"
+    }    
 }
-else
-{
-    Write-Host "ANTLR is not installed. Follow the instructions at https://github.com/antlr/antlr4/blob/master/doc/getting-started.md"
+finally {
+    Pop-Location
 }
-
