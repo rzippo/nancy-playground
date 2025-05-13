1d323f604b5a9c9570ed691671a1f88b366e7c65
Raffaele Zippo <raffaele.zippo@ing.unipi.it>
2025-05-13 19:15:37 +0200

    Implement parsing of upp()

--- Nancy-Playground/MppgParser/Visitors/ExpressionVisitor.cs
+++ Nancy-Playground/MppgParser/Visitors/ExpressionVisitor.cs
@@ -40,38 +40,9 @@ public partial class ExpressionVisitor : MppgBaseVisitor<IExpression>
 
     public override IExpression VisitNumberLiteralExp(Grammar.MppgParser.NumberLiteralExpContext context)
     {
-        var numberText = context.GetText();
-        Rational value;
-        if (numberText.Contains("inf") || numberText.Contains("infinity") || numberText.Contains("Infinity"))
-        {
-            numberText = numberText.Replace("inf", string.Empty).Trim();
-            if(numberText.Length == 0 || numberText == "+")
-                value = Rational.PlusInfinity;
-            else if (numberText == "-")
-                value = Rational.MinusInfinity;
-            else
-                throw new Exception($"Invalid number literal: {numberText}");
-        }
-        else if(numberText.Contains('.'))
-        {
-            value = decimal.Parse(numberText, CultureInfo.InvariantCulture);
-        }
-        else if(numberText.Contains('/'))
-        {
-            var split = numberText.Split("/");
-            var numerator = int.Parse(split[0], CultureInfo.InvariantCulture);
-            var denominator = int.Parse(split[1], CultureInfo.InvariantCulture);
-            value = new Rational(numerator, denominator);
-        }
-        else if (int.TryParse(numberText, NumberStyles.Number, CultureInfo.InvariantCulture, out var numerator))
-        {
-            value = numerator;
-        }
-        else
-        {
-            throw new Exception($"Invalid number literal: {numberText}");
-        }
-
+        var numberLiteralVisitor = new NumberLiteralVisitor();
+        var value = numberLiteralVisitor.Visit(context);
+        
         var valueExp = Expressions.FromRational(value, "");
         return valueExp;
     }
