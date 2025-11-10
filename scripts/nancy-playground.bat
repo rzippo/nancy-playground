9e4ebc4b45c56f0db0ffb0796f86a4c80858e6b1
Raffaele Zippo <raffaele.zippo@ing.unipi.it>
2025-11-10 11:28:04 +0100

    Add install scripts

---
+++ scripts/nancy-playground.bat
@@ -0,0 +1,3 @@
+@echo off
+set args="nancy-playground %1"
+pwsh -Command %args%
---
+++ scripts/nancy-playground.ps1
@@ -0,0 +1,17 @@
+Param(
+    [switch]$DebugConfig = $false,
+    [switch]$RunVerbose = $false
+);
+
+$projectPath = "!! REPLACE projectPath !!";
+
+$configuration = $DebugConfig ? "Debug" : "Release";
+$runVerbosity = $RunVerbose ? "detailed" : "quiet";
+
+$scriptParameters = $MyInvocation.MyCommand.Parameters.Keys;
+$programArgs = $args | Where-Object { -not ( $scriptParameters -contains $_ ) } | ForEach-Object { "`"$_`"" };
+$joinedProgramArgs = $programArgs -join " ";
+
+dotnet restore $projectPath --verbosity $runVerbosity --nologo;
+dotnet build $projectPath --configuration $configuration --verbosity $runVerbosity --no-restore --nologo;
+Invoke-Expression "dotnet run --configuration $configuration --project $projectPath --verbosity $runVerbosity --no-restore --no-build -- $joinedProgramArgs";
