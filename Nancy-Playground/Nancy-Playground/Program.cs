5805b1a41bf754befae9a9da8ce67347044d9865
Raffaele Zippo <raffaele.zippo@outlook.it>
2025-11-11 07:55:14 +0000

    Add --help documentation

--- Nancy-Playground/Nancy-Playground/Program.cs
+++ Nancy-Playground/Nancy-Playground/Program.cs
@@ -7,8 +7,14 @@ CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 var app = new CommandApp<InteractiveCommand>();
 app.Configure(config =>
 {
-    config.AddCommand<RunCommand>("run");
-    config.AddCommand<InteractiveCommand>("interactive");
-    config.AddCommand<SetupCommand>("setup");
+    config.AddCommand<RunCommand>("run")
+        .WithDescription("Runs a .mppg script")
+        .WithExample("run", "./Examples/hal-04513292v1.mppg")
+        .WithExample("run", "./Examples/hal-04513292v1.mppg", "--output-mode", "MppgClassic", "--run-mode", "PerStatement")
+        .WithExample("run", "./Examples/hal-04513292v1.mppg", "--output-mode", "NancyNew", "--run-mode", "ExpressionsBased");
+    config.AddCommand<InteractiveCommand>("interactive")
+        .WithDescription("Interactive mode, where the user can input MPPG lines one by one.");
+    config.AddCommand<SetupCommand>("setup")
+        .WithDescription("Initializes dependencies. Required to enable exporting plots to images.");
 });
 return app.Run(args);
