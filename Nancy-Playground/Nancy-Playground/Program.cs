61690e873859a80d115c97504b3494bedd9828bc
Raffaele Zippo <raffaele.zippo@outlook.it>
2025-11-13 15:39:14 +0000

    Propagate exceptions in debug mode

--- Nancy-Playground/Nancy-Playground/Program.cs
+++ Nancy-Playground/Nancy-Playground/Program.cs
@@ -16,5 +16,9 @@ app.Configure(config =>
         .WithDescription("Interactive mode, where the user can input MPPG lines one by one.");
     config.AddCommand<SetupCommand>("setup")
         .WithDescription("Initializes dependencies. Required to enable exporting plots to images.");
+
+    #if DEBUG
+    config.PropagateExceptions();
+    #endif
 });
 return app.Run(args);
