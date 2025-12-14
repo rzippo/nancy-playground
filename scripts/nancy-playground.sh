34d14b169fd20f4faa161ab1c70a3706c9a75713
Raffaele Zippo <raffaele.zippo@outlook.it>
2025-12-14 23:27:24 +0100

    install: improve linux dev launch script

--- scripts/nancy-playground.sh
+++ scripts/nancy-playground.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-psargs="nancy-playground.ps1 $*"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+psargs="$SCRIPT_DIR/nancy-playground.ps1 $*"
 pwsh -Command "$psargs"
