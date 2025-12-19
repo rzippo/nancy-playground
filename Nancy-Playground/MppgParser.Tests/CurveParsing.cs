18e9af9e1536c5af9a8d2c8020a28be52c7f6bbd
Raffaele Zippo <raffaele.zippo@ing.unipi.it>
2025-12-19 17:41:44 +0000

    Add basic curve tests

--- Nancy-Playground/MppgParser.Tests/CurveParsing.cs
+++ Nancy-Playground/MppgParser.Tests/CurveParsing.cs
@@ -21,6 +21,89 @@ public class CurveParsing
             new RateLatencyServiceCurve(1, 2)
         ),
         (
+            "ratency(1, 0)",
+            new RateLatencyServiceCurve(1, 0)
+        ),
+        (
+            "ratency(0, 2)",
+            new RateLatencyServiceCurve(0, 2)
+        ),
+        (
+            "ratency(0, 0)",
+            new RateLatencyServiceCurve(0, 0)
+        ),
+        (
+            "ratency(0, 0)",
+            Curve.Zero()
+        ),
+        (
+            "bucket(2, 3)",
+            new SigmaRhoArrivalCurve(3, 2)
+        ),
+        (
+            "affine(2, 3)",
+            new Curve(
+                new Sequence([
+                    new Point(0, 3),
+                    new Segment(0, 1, 3, 2)
+                ]),
+                0, 1, 2
+            )
+        ),
+        (
+            "affine(2, 0)",
+            new Curve(
+                new Sequence([
+                    new Point(0, 0),
+                    new Segment(0, 1, 0, 2)
+                ]),
+                0, 1, 2
+            )
+        ),
+        (
+            "affine(0, 2)",
+            new Curve(
+                new Sequence([
+                    new Point(0, 2),
+                    new Segment(0, 1, 2, 0)
+                ]),
+                0, 1, 0
+            )
+        ),
+        (
+            "step(5, 10)",
+            new StepCurve(10, 5)
+        ),
+        (
+            "step(0, 10)",
+            new StepCurve(10, 0)
+        ),
+        (
+            "stair(2, 3, 4)",
+            new StairCurve(4, 3).DelayBy(2)
+        ),
+        (
+            "stair(0, 3, 4)",
+            new StairCurve(4, 3)
+        ),
+        // todo: constant functions are implicitly constructed, how to test them?
+        (
+            "delay(7)",
+            new DelayServiceCurve(7)
+        ),
+        (
+            "delay(0)",
+            new DelayServiceCurve(0)
+        ),
+        (
+            "zero",
+            Curve.Zero()
+        ),
+        (
+            "epsilon",
+            Curve.PlusInfinite()
+        ),
+        (
             "upp( period ( [(0, 0) 0 (2, 0)[ [(2, 0) 1 (7, 5)] ](7, 5) 0 ( 12, 5 )[ ))",
             new Curve(
                 new Sequence([
