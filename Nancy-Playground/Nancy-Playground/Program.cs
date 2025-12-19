8efa2b1cc8d0cdf9272b3b0859f3f4ba8748a403
Raffaele Zippo <raffaele.zippo@ing.unipi.it>
2025-12-19 09:58:46 +0000

    run: support output redirection, add --deterministic flag

--- Nancy-Playground/Nancy-Playground/Program.cs
+++ Nancy-Playground/Nancy-Playground/Program.cs
@@ -1,35 +1,48 @@
-﻿
-using System.Globalization;
+﻿using System.Globalization;
 using Spectre.Console;
 using Spectre.Console.Cli;
-using Unipi.Nancy.Playground.Cli;
 
-CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+namespace Unipi.Nancy.Playground.Cli;
 
-// CLI welcome message
-AnsiConsole.MarkupLine("[green]This is [blue]nancy-playground[/], version 1.0.0.[/]");
-// todo: add reference to the maintainer somewhere?
-AnsiConsole.MarkupLine("[green]Academic attribution: if useful, please cite [yellow]https://doi.org/10.1016/j.softx.2022.101178[/][/]");
-
-var app = new CommandApp<InteractiveCommand>();
-app.Configure(config =>
+internal class Program
 {
-    config.AddCommand<RunCommand>("run")
-        .WithDescription("Runs a .mppg script")
-        .WithExample("run", "./Examples/hal-04513292v1.mppg")
-        .WithExample("run", "./Examples/hal-04513292v1.mppg", "--output-mode", "MppgClassic", "--run-mode", "PerStatement")
-        .WithExample("run", "./Examples/hal-04513292v1.mppg", "--output-mode", "NancyNew", "--run-mode", "ExpressionsBased");
+    public static List<string> CliWelcomeMessage =
+    [
+        "[green]This is [blue]nancy-playground[/], version 1.0.0.[/]",
+        // todo: add reference to the maintainer somewhere?
+        "[green]Academic attribution: if useful, please cite [yellow]https://doi.org/10.1016/j.softx.2022.101178[/][/]"
+    ];
     
-    config.AddCommand<InteractiveCommand>("interactive")
-        .WithDescription("Interactive mode, where the user can input MPPG lines one by one.");
+    public static int Main(string[] args)
+    {
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        if(Console.IsOutputRedirected)
+            AnsiConsole.Profile.Capabilities.Ansi = false;
 
-    config.AddCommand<ConvertCommand>("convert");
+        foreach (var cliWelcomeLine in CliWelcomeMessage)
+            AnsiConsole.MarkupLine(cliWelcomeLine);
+
+        var app = new CommandApp<InteractiveCommand>();
+        app.Configure(config =>
+        {
+            config.AddCommand<RunCommand>("run")
+                .WithDescription("Runs a .mppg script")
+                .WithExample("run", "./Examples/hal-04513292v1.mppg")
+                .WithExample("run", "./Examples/hal-04513292v1.mppg", "--output-mode", "MppgClassic", "--run-mode", "PerStatement")
+                .WithExample("run", "./Examples/hal-04513292v1.mppg", "--output-mode", "NancyNew", "--run-mode", "ExpressionsBased");
     
-    config.AddCommand<SetupCommand>("setup")
-        .WithDescription("Initializes dependencies. Required to enable exporting plots to images.");
+            config.AddCommand<InteractiveCommand>("interactive")
+                .WithDescription("Interactive mode, where the user can input MPPG lines one by one.");
 
-    #if DEBUG
-    config.PropagateExceptions();
-    #endif
-});
-return app.Run(args);
+            config.AddCommand<ConvertCommand>("convert");
+    
+            config.AddCommand<SetupCommand>("setup")
+                .WithDescription("Initializes dependencies. Required to enable exporting plots to images.");
+
+#if DEBUG
+            config.PropagateExceptions();
+#endif
+        });
+        return app.Run(args);
+    }
+}
