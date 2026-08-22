ccd271e0aa985fc9df38ccd7b7b7aadb04bb7aa9
Raffaele Zippo <raffaele.zippo@ing.unipi.it>
2026-08-22 09:57:39 +0200

    parser: drop the unused expression parse duplicate
    Expression.ParseFromString had no caller, ExpressionParsing.Parse being the one used, and the parser ships inside the CLI rather than as a library.

--- Nancy-Playground/MppgParser/Statements/Expression.cs
+++ Nancy-Playground/MppgParser/Statements/Expression.cs
@@ -62,28 +62,6 @@ public class Expression
         return expression;
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="expression"></param>
-    /// <param name="state"></param>
-    /// <returns>
-    /// This method returns *either* a <see cref="CurveExpression"/>, if the expression resolves to a function,
-    /// *or* a <see cref="RationalExpression"/> if the expression resolves to a number.
-    /// The returned tuple will have null for the other type.  
-    /// </returns>
-    public static IExpression ParseFromString(string expression, State? state, SyntaxVersion syntaxVersion = default)
-    {
-        var version = syntaxVersion == default ? SyntaxVersion.Latest : syntaxVersion;
-        var parse = MppgParsing.Create(expression, ErrorRecovery.FirstError, version, state);
-
-        // the entry rule is anchored at EOF, so input left over after the expression is reported
-        var context = parse.ParseOrThrow(static parser => parser.expressionEntry()).expression();
-        var visitor = new ExpressionVisitor(state);
-
-        return context.Accept(visitor);
-    }
-
     public void ParseTree(State state)
     {
         switch (SourceType)
